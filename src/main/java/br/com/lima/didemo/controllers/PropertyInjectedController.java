8199e981cefe81dd2aba5112a82f7ff6c12f9400
Rafael Lima <rafael.lima@ciag.org.br>
2018-01-24 13:40:59 -0200

    inject by service

--- src/main/java/br/com/lima/didemo/controllers/PropertyInjectedController.java
+++ src/main/java/br/com/lima/didemo/controllers/PropertyInjectedController.java
@@ -1,5 +1,6 @@
 package br.com.lima.didemo.controllers;
 
+import br.com.lima.didemo.services.GreetingService;
 import br.com.lima.didemo.services.GreetingServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,10 +9,10 @@ import org.springframework.stereotype.Controller;
 public class PropertyInjectedController {
 
     @Autowired
-    public GreetingServiceImpl greetingService;
+    public GreetingService greetingServiceImpl ;
 
     public String sayHello(){
-        return greetingService.sayGreeting();
+        return greetingServiceImpl .sayGreeting();
     }
 
 }
